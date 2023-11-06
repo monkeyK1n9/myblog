11b3d9f23e069474984e86936d8a515004469b0f
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-11-06 10:13:53 +0100

    restyling old blog site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import Navbar from './components/Navbar'
-import Profile from './components/Profile'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -17,8 +16,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="dark:bg-slate-800">
         <Navbar />
-        <Profile />
-        {children}
+        <main className='px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto'>
+          {children}
+        </main>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image'
 import Posts from './components/Posts'
+import Profile from './components/Profile'
 
 export default function Home() {
   return (
-    <main className="px-6 mx-auto">
+    <div className="mx-auto">
+      <Profile />
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and welcome 🖐
         <span className="whitespace-nowrap">
-          I&apos;m <span className="font-bold">King</span>
+          I'm <span className="font-bold">King</span>
         </span>
       </p>
 
       <Posts />
-    </main>
+    </div>
   )
 }
